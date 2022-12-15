7431914ca4c3ff80e66af6c4b26f34bbd2bea8a4
Inusha Peiris <inushaumayanthapeiris@gmail.com>
2022-12-16 01:25:57 +0530

    Implementing frontend

---
+++ Web Application/FRONTEND/admin-side-webapp/src/App.js
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import {BrowserRouter as Router, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        hello world
+      </div>
+      
+    </Router>
+  );
+}
+
+export default App;
