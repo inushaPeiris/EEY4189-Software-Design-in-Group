7e9d72ea051dd32f5e36d8c2caa9d18fac0e154f
Inusha Peiris <101092263+inushaPeiris@users.noreply.github.com>
2023-11-28 12:00:52 +0530

    inusha

---
+++ Mobile App/api/index.js
@@ -0,0 +1,155 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+const port = 9000;
+const cors = require("cors");
+app.use(cors());
+
+//to access the request's data easily
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://travelit:eHBu8pL1q4qHAdGv@cluster0.sul9fgc.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error Connecting to MongoDB");
+  });
+
+app.listen(port, () => {
+  console.log("server is running on port 9000");
+});
+
+const User = require("./models/user");
+
+//endpoint to register a user in the backend
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already registered" });
+    }
+
+    //create a new user
+    const newUser = new User({ name, email, password });
+
+    //generate and store the verification token
+    newUser.verificationToken = crypto.randomBytes(20).toString("hex");
+
+    //save the  user to the database
+    await newUser.save();
+
+    //send the verification email to the user
+    sendVerificationEmail(newUser.email, newUser.verificationToken);
+
+    res.status(200).json({ message: "Registration successful" });
+  } catch (error) {
+    console.log("error registering user", error);
+    res.status(500).json({ message: "error registering user" });
+  }
+});
+
+const sendVerificationEmail = async (email, verificationToken) => {
+  //create a nodemailer transporter
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "teamjustnerds@gmail.com",
+      pass: "miptnxktvnjlgcla",
+    },
+  });
+
+  //compose the email message
+  const mailOptions = {
+    from: "Travelit.com",
+    to: email,
+    subject: "Email Verification",
+    text: `please click the following link to verify your email http://localhost:9000/verify/${verificationToken}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.log("error sending email", error);
+  }
+};
+
+app.get("/verify/:token", async (req, res) => {
+  try {
+    const token = req.params.token;
+
+    const user = await User.findOne({ verificationToken: token });
+    if (!user) {
+      return res.status(404).json({ message: "Invalid token" });
+    }
+
+    user.verified = true;
+    user.verificationToken = undefined;
+    await user.save();
+
+    res.status(200).json({ message: "Email verified successfully" });
+  } catch (error) {
+    console.log("error getting token", error);
+    res.status(500).json({ message: "Email verification failed" });
+  }
+});
+
+const generateSecretKey = () => {
+  const secretKey = crypto.randomBytes(32).toString("hex");
+  return secretKey;
+};
+
+const secretKey = generateSecretKey();
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "Invalid email" });
+    }
+
+    if (user.password !== password) {
+      return res.status(404).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, secretKey);
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Login failed" });
+  }
+});
+
+app.get("/profile/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ message: "Error while getting the profile" });
+  }
+});
---
+++ Mobile App/frontend/src/screens/authentication/Login.js
@@ -0,0 +1,195 @@
+import React, { useState, useEffect } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  StyleSheet,
+  ImageBackground,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+
+const Login = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //user will be navigating to the dashboard if they are registered.
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const token = await AsyncStorage.getItem("authToken");
+
+        if (token) {
+          setTimeout(() => {
+            navigation.replace("Dashboard");
+          }, 600);
+        }
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
+
+  const handleLogin = () => {
+    const user = {
+      email: email,
+      password: password,
+    };
+
+    axios
+      .post("http://localhost:9000/login", user)
+      .then((response) => {
+        console.log(response);
+        const token = response.data.token;
+        AsyncStorage.setItem("authToken", token);
+        navigation.navigate("Dashboard");
+      })
+      .catch((error) => {
+        alert("Login error");
+        console.log("error ", error);
+      });
+  };
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        {/* background */}
+        <View style={style.container}>
+          <ImageBackground
+            source={require("../../assest/images/login_bg.png")}
+            resizeMode="cover"
+            style={style.defaultbg}
+          >
+            <Image
+              source={require("../../assest/images/travelit_logo.png")}
+              style={{
+                alignItems: "center",
+                width: 150,
+                height: 150,
+                padding: 20,
+                resizeMode: "contain",
+                alignSelf: "center",
+              }}
+            />
+          </ImageBackground>
+        </View>
+
+        {/* Text Input Fields */}
+        <View style={style.formInput}>
+          <TextInput
+            style={style.textInput}
+            placeholder="Email address"
+            keyboardType="email-address"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+        </View>
+        <View style={style.formInput}>
+          <TextInput
+            style={style.textInput}
+            placeholder="Password"
+            secureTextEntry={true}
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+        </View>
+
+        <View style={style.formInput}>
+          <TouchableOpacity>
+            <Text
+              style={{
+                color: "#d10202",
+                fontSize: 16,
+                fontWeight: "500",
+                textAlign: "right",
+              }}
+              onPress={() => navigation.navigate("Forgotpw")}
+            >
+              {" "}
+              Fogot Password?
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Main Button */}
+        <View style={style.formInput}>
+          <TouchableOpacity style={style.mainButton} onPress={handleLogin}>
+            <Text
+              style={{
+                textAlign: "center",
+                fontSize: 16,
+                fontWeight: "500",
+                color: "white",
+              }}
+            >
+              Login
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={style.formInput}>
+          <Text style={{ textAlign: "center" }}>or</Text>
+        </View>
+
+        {/* Registration */}
+        <View style={style.formInput}>
+          <TouchableOpacity
+            style={style.mainButton}
+            onPress={() => navigation.navigate("Register")}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                fontSize: 16,
+                fontWeight: "500",
+                color: "white",
+              }}
+            >
+              Register
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  defaultbg: {
+    width: "100%",
+    height: 140,
+  },
+
+  formInput: {
+    marginTop: 10,
+    padding: 10,
+  },
+
+  textInput: {
+    padding: 10,
+    fontSize: 16,
+    borderWidth: 1,
+    borderColor: "#477173",
+    borderRadius: 10,
+  },
+
+  mainButton: {
+    padding: 12,
+    backgroundColor: "#076ff7",
+    borderRadius: 10,
+  },
+});
+export default Login;
